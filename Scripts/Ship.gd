a62dc0bb0078a6d03c53e9b727e4c1b9687b5ebb
Jocelyn Beedie <jellonator00@gmail.com>
2020-02-12 16:16:40 -0500

    Did thing

--- Scripts/Ship.gd
+++ Scripts/Ship.gd
@@ -1,8 +1,16 @@
 extends KinematicBody2D
 
+const SPEED = 600
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("left"):
+		self.position.x -= delta * SPEED
+	if Input.is_action_pressed("right"):
+		self.position.x += delta * SPEED
+	if Input.is_action_pressed("up"):
+		self.position.y -= delta * SPEED
+	if Input.is_action_pressed("down"):
+		self.position.y += delta * SPEED
